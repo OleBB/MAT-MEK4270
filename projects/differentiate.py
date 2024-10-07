5a146fd819e327831f719c3e8e772a0fe102977b
OleBB <oleb1996@gmail.com>
2024-10-07 11:20:24 +0200

    differentiate.py is done

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,21 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    N = len(u)
+    du = np.zeros(N)
+    du[0] = (u[1]-u[0])/dt
+    du[-1] = (u[-1]-u[-2])/dt
+    for i in range(1,N-1):
+        du[i] = (u[i+1]-u[i-1])/(2*dt)
+    return du
 
 def differentiate_vector(u, dt):
-    pass
+    N = len(u)
+    du = np.zeros(N)
+    du[1:-1] = (u[2:]-u[:-2])/(2*dt)
+    du[0] = (u[1]-u[0])/dt
+    du[-1] = (u[-1]-u[-2])/dt
+    return du
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
